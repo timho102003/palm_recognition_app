c66efd2a22c7599beb33f7f97be34a52e35d577e
timho <timho102003@gmail.com>
2023-04-16 00:10:23 -0700

    add model performance page

---
+++ pages/4_Model_Performance.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import streamlit as st
+from streamlit_extras.dataframe_explorer import dataframe_explorer
+
+st.set_page_config(page_title="Model Performance")
+st.markdown("# Model Performance Detail")
+st.divider()
+
+@st.cache_data
+def load_performance(assets="./assets/streamlit_model_perf.csv"):
+    return pd.read_csv(assets)
+@st.cache_data
+def load_raw_df(assets="./assets/raw_results.csv"):
+    return pd.read_csv(assets)
+
+perf_table = load_performance()
+raw_table = load_raw_df()
+st.area_chart(perf_table, x="Threshold", y=["TPR", "FPR"], use_container_width=True)
+df_exp_obj = dataframe_explorer(raw_table, case=False)
+st.dataframe(df_exp_obj, use_container_width=True)
