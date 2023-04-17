42a8c6ff9112fa8b2374544af58aa45da563e438
timho <timho102003@gmail.com>
2023-04-17 01:24:39 -0700

    update SUPER condition handling

--- pages/1_Register.py
+++ pages/1_Register.py
@@ -1,3 +1,4 @@
+import os
 import time
 from PIL import Image
 import streamlit as st
@@ -26,7 +27,7 @@ with in_col1:
 with in_col2:
     my_upload = st.camera_input("Take a picture")
 
-if st.session_state["SUPER"]:
+if os.environ["SUPER"] == "True":
     col1, col2, col3, col4 = st.columns(4)
 else:
     col1, col2= st.columns(2)
@@ -48,7 +49,7 @@ if my_upload is not None:
     rm_bg = remove_bg(image=image)
     rm_bg_end = time.time()
 
-    if st.session_state["SUPER"]:
+    if os.environ["SUPER"] == "True":
         col2.write("Remove Background")
         col2.caption("elapse time: {:.3f} ms".format((rm_bg_end - rm_bg_start) * 1000))
         col2.image(rm_bg)
@@ -57,14 +58,14 @@ if my_upload is not None:
     keypoints, keypoint_plot = detect_keypoints(rm_bg=rm_bg)
     pt_det_end = time.time()
     if keypoints is not None:
-        if st.session_state["SUPER"]:
+        if os.environ["SUPER"] == "True":
             col3.write("Keypoint Detection")
             col3.caption("elapse time: {:.3f} ms".format((pt_det_end - pt_det_start) * 1000))
             col3.image(keypoint_plot)
         norm_start = time.time()
         rot_angle, norm_img = normalize_img(image=rm_bg, points=keypoints)
         norm_end = time.time()
-        if st.session_state["SUPER"]:
+        if os.environ["SUPER"] == "True":
             col4.write("Normalize: ")
             col4.caption(
                 "elapse time: {:.3f} ms, rotate: {:.1f} deg".format(
@@ -81,7 +82,7 @@ if my_upload is not None:
             )
             col2.image(norm_img)
     else:
-        if st.session_state["SUPER"]:
+        if os.environ["SUPER"] == "True":
             col3.write("Keypoint Detection")
             col3.warning("Fail to detect keypoints")
             col4.write("Normalize: ")
@@ -100,7 +101,7 @@ if my_upload is not None:
         end_ext = time.time()
 
     if feature is not None:
-        if st.session_state["SUPER"]:
+        if os.environ["SUPER"] == "True":
             st.info("Elapse Time: {:.2f} ms".format((end_ext - start_ext) * 1000), icon="🔥")
         else:
             tot_time_spend = (rm_bg_end - rm_bg_start) + \
--- pages/3_Manage_Feature_DB.py
+++ pages/3_Manage_Feature_DB.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 from utils import center_image
 from streamlit_extras.customize_running import center_running
@@ -7,40 +8,37 @@ st.set_page_config(page_title="Feature DB Management")
 st.markdown("# Manage Feature DB")
 st.divider()
 
-if "SUPER" not in list(st.session_state.keys()):
-    center_running()
-else:
-    if st.session_state["SUPER"]:
-        st.write("Delete User:")
-        username = st.text_input(
-                    "Enter User ID 👇",
-                    label_visibility="visible",
-                    disabled=False,
-                    placeholder="YOUR-ID",
-                )
-        if username:
-            isexist_check = len(st.session_state["index"].fetch(
-                    ids=[f"streamlit_user.{username.lower()}"]
-                )["vectors"])
-            if isexist_check:
-                st.success(f"The user {username} is exist")
-                if button("Delete User", key="deleteUser"):
-                    if button("Go Ahead", key="doublecheck"):
-                        try:
-                            st.session_state["index"].delete(ids=[f"streamlit_user.{username.lower()}"], namespace='')
-                            isexist_check_2 = len(st.session_state["index"].fetch(ids=[f"streamlit_user.{username.lower()}"])["vectors"])
-                            st.write(isexist_check_2)
-                            if isexist_check_2:
-                                st.error("Something went wrong when deleting user!!!")
-                            else:
-                                st.success("Successfully delete user {username}")
-
-                        except Exception as e:
-                            st.error(e)
+if os.environ["SUPER"] == "True":
+    st.write("Delete User:")
+    username = st.text_input(
+                "Enter User ID 👇",
+                label_visibility="visible",
+                disabled=False,
+                placeholder="YOUR-ID",
+            )
+    if username:
+        isexist_check = len(st.session_state["index"].fetch(
+                ids=[f"streamlit_user.{username.lower()}"]
+            )["vectors"])
+        if isexist_check:
+            st.success(f"The user {username} is exist")
+            if button("Delete User", key="deleteUser"):
+                if button("Go Ahead", key="doublecheck"):
+                    try:
+                        st.session_state["index"].delete(ids=[f"streamlit_user.{username.lower()}"], namespace='')
+                        isexist_check_2 = len(st.session_state["index"].fetch(ids=[f"streamlit_user.{username.lower()}"])["vectors"])
+                        st.write(isexist_check_2)
+                        if isexist_check_2:
                             st.error("Something went wrong when deleting user!!!")
-            else:
-                st.error(f"The user {username} does not exist, please check your username input")
-    else:
-        st.error("You have to be a superuser to manage the feature database!")
-        center_image("assets/sad_face.png")
+                        else:
+                            st.success("Successfully delete user {username}")
+
+                    except Exception as e:
+                        st.error(e)
+                        st.error("Something went wrong when deleting user!!!")
+        else:
+            st.error(f"The user {username} does not exist, please check your username input")
+else:
+    st.error("You have to be a superuser to manage the feature database!")
+    center_image("assets/sad_face.png")
 
